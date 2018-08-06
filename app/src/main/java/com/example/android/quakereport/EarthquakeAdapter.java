89d6d6d5a167717d49b47c56c2026eab11349a76
brendadev <brenda@koneksahealth.com>
2018-08-06 00:26:28 -0400

    added switch statement to change color of magnitude circle

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.quakereport;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import android.graphics.drawable.GradientDrawable;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     public EarthquakeAdapter(Activity context, ArrayList<Earthquake> earthquakes) {
@@ -32,6 +34,16 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         DecimalFormat formatter = new DecimalFormat("0.0");
         String output = formatter.format(currentEarthquake.getMagnitude());
         earthquakeMagnitudeTextView.setText(output);
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) earthquakeMagnitudeTextView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
 
         // Find the textView in the list_item.xml layout with the ID quake_proximity
         TextView earthquakeProximityTextView = (TextView) listItemView.findViewById(R.id.quake_proximity);
@@ -60,5 +72,46 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         earthquakeTimeTextView.setText(currentEarthquake.getTime());
 
         return listItemView;
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+        // Anything above a magnitude 10 earthquake uses the darkest red color (#C03823).
+        // Anything between a magnitude 9 and 10 earthquake uses a slightly lighter red color (#D93218),
+        // anything between a magnitude 8 and 9 earthquake uses an even lighter red color (#E13A20),
+        // and so on. Each level has a different color. Anything below a 2.0 earthquake uses a blue
+        // color (#4A7BA7). The specific hex values are provided below.
+        int magnitudeColorInt;
+        int magnitudeRoundedDown = (int) Math.floor(magnitude);
+        switch (magnitudeRoundedDown) {
+            case 0:
+            case 1:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude1);
+                break;
+            case 3:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude3);
+                break;
+            case 4:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude4);
+                break;
+            case 5:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude5);
+                break;
+            case 6:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude6);
+                break;
+            case 7:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude7);
+                break;
+            case 8:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude8);
+                break;
+            case 9:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude9);
+                break;
+            default:
+                magnitudeColorInt = ContextCompat.getColor(getContext(), R.color.magnitude10plus);
+                break;
+        }
+        return magnitudeColorInt;
     }
 }
