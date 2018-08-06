cfbc5423a95f639cda1eae8aa5507f00a0dd3cb9
brendadev <brenda@koneksahealth.com>
2018-08-05 22:21:29 -0400

    able to render static earthquake data

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -2,6 +2,9 @@ package com.example.android.quakereport;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
@@ -10,8 +13,22 @@ public class EarthquakeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.list, new EarthquakeFragment())
-                .commit();
+
+        // Create ArrayList of earthquake objects
+        final ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
+        earthquakes.add(new Earthquake("6.0", "San Francisco", "8:30AM"));
+        earthquakes.add(new Earthquake("6.0", "London", "8:30AM"));
+        earthquakes.add(new Earthquake("6.0", "Tokyo", "8:30AM"));
+        earthquakes.add(new Earthquake("6.0", "Mexico City", "8:30AM"));
+        earthquakes.add(new Earthquake("6.0", "Moscow", "8:30AM"));
+        earthquakes.add(new Earthquake("6.0", "Rio de Janeiro", "8:30AM"));
+        earthquakes.add(new Earthquake("6.0", "Paris", "8:30AM"));
+
+        EarthquakeAdapter itemsAdapter = new EarthquakeAdapter(this, earthquakes);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        earthquakeListView.setAdapter(itemsAdapter);
     }
 }
