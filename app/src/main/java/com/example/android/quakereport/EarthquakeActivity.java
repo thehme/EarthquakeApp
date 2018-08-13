448116a56c9b64ca1ed9c86d53131b5778f9a210
brendadev <brenda@koneksahealth.com>
2018-08-13 08:49:18 -0400

    added async method to fetch earthquake data and changed methods to use this data instead of the previously fetched data

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -2,6 +2,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,8 +10,12 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
+    /** Adapter for the list of earthquakes */
+    private EarthquakeAdapter itemsAdapter;
+
     /** URL for earthquake data from the USGS dataset */
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
@@ -20,25 +25,43 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create ArrayList of earthquake objects
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
-        EarthquakeAdapter itemsAdapter = new EarthquakeAdapter(this, earthquakes);
+        itemsAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         earthquakeListView.setAdapter(itemsAdapter);
 
+        FetchEarthquakeDataAsyncTask task = new FetchEarthquakeDataAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Earthquake currentEarthquake = earthquakes.get(position);
+                Earthquake currentEarthquake = itemsAdapter.getItem(position);
                 String url = currentEarthquake.getUrl();
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
             }
         });
+    }
+
+    private class FetchEarthquakeDataAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(urls[0]);
+            return earthquakes;
+        }
+        protected void onPostExecute(List<Earthquake> earthquakes) {
+            // clear adapter first
+            itemsAdapter.clear();
+            if (earthquakes != null && !earthquakes.isEmpty()) {
+                itemsAdapter.addAll(earthquakes);
+            }
+        }
     }
 }
